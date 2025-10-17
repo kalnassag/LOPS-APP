8e144a06df57b396c290b664af2c8cbdd2f559e2
Kareem <kalnassag@proton.me>
2025-10-17 22:05:43 +1000

    Added descriptions to functions

--- utilities.py
+++ utilities.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 
 def normalise_path_to_properties(col):
+    """
+    Reads the JSON file and converts the dot notation (e.g. Product.Model Name) to
+    an appropriate snake case property name (product_model_name)
+    """
     pattern = r'[\s().]+'
     normalised_item = re.sub(pattern, "_", str(col))
     normalised_item = re.sub(r'_+', '_', normalised_item)
@@ -11,10 +15,14 @@ def normalise_path_to_properties(col):
         normalised_item = "has" + normalised_item.lstrip("no.")
     return normalised_item
 
-def map_properties():
+def map_properties(sample_file):
+    """"
+    Maps property names from the json files to the normalised property name as they should appear in the graph.
+    Check the files *_property_mapping.json to see how tha looks like.
+    """
     property_mapping = {}
-    with open('smartwatch_sample.json', 'r') as f:
-        file = json.load(f)
+    with open(sample_file, 'r') as sf:
+        file = json.load(sf)
         data = pd.json_normalize(file)
         for col in data.columns:
             if col.startswith('No.'):
@@ -23,11 +31,9 @@ def map_properties():
             property_mapping[col] = graph_property
     return property_mapping
 
-prop_mapping = map_properties()
-
 def save_mapping(prop_mapping, filename='laptop_property_mapping.json'):
     with open(filename, 'w') as f:
         json.dump(prop_mapping, f, indent=2)
         print("succeeded")
 
-save_mapping(prop_mapping, filename = 'smartwatch_property_mapping.json')
+save_mapping(map_properties('smartwatch_property_mapping.json'), filename = 'smartwatch_property_mapping.json')
